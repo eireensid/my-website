41d90d70290849be332970820d856e317144b2aa
Irina <sidoren-irina@yandex.ru>
2020-04-12 13:32:32 +0300

    all bugs are fixed

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        nav:true,
-        responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:1
-          },
-          1000:{
-              items:1
-          }
-        }
-    })
+document.addEventListener('DOMContentLoaded', function(){
+    // Clear url from anchor
+    var url = window.location.href
+    if (url.includes('#')) {
+      url = url.replace(/#.*/, '')
+      location.replace(url)
+    }
 
+    // Scroll effect
     window.addEventListener('scroll', function(e) {
       var mouse = document.querySelector('.mouse')
       mouse.style.display = 'block'
       setTimeout(function() {
         mouse.style.display = 'none'
       }, 1000)
-      // console.log('onScrollEvent isBottom', isBottom)
     })
 
     // Toggle button
     document.querySelector('.mobile-menu-button').addEventListener('click', function() {
-      let menu = document.querySelector('.mobile-menu-block')
+      var menu = document.querySelector('.mobile-menu-block')
       menu.classList.toggle('mobile-menu-block-visible')
     })
-});
+
+    // Close mobile menu after going to link
+    var links = document.querySelectorAll('.mobile-menu .menu-item a') 
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", function() {
+        var menu = document.querySelector('.mobile-menu-block')
+        menu.classList.remove('mobile-menu-block-visible')
+      })
+    }
+
+    // Carousel
+    function setNewVideoSrcByInd(ind) {
+      var newSrc = videos[ind]
+      var iframe = document.getElementById('video1') 
+      // свойство src найденного элемента меняется на значение массива по новому индексу
+      iframe.src = newSrc
+    }
+    var videos = [
+      'https://www.youtube.com/embed/9NjhChKMjFM',
+      'https://www.youtube.com/embed/CaBioMr4qcs',
+      'https://www.youtube.com/embed/xjK1Oincr9w',
+      'https://www.youtube.com/embed/TAJim20qnd8'
+    ]
+    var curInd = 0
+    document.querySelector('.btn-next').addEventListener('click', function() {
+      curInd = (curInd + 1) % videos.length
+      // src video в начале = нулевой элемент массива
+      setNewVideoSrcByInd(curInd)
+    })
+    document.querySelector('.btn-back').addEventListener('click', function() {
+      curInd = curInd - 1
+      if (curInd < 0) {
+        curInd = videos.length - 1
+      }
+      setNewVideoSrcByInd(curInd)
+    })
+    document.querySelector('.btn-right').addEventListener('click', function() {
+      curInd = (curInd + 1) % videos.length
+      // src video в начале = нулевой элемент массива
+      setNewVideoSrcByInd(curInd)
+    })
+    document.querySelector('.btn-left').addEventListener('click', function() {
+      curInd = curInd - 1
+      if (curInd < 0) {
+        curInd = videos.length - 1
+      }
+      setNewVideoSrcByInd(curInd)
+    })
+})
